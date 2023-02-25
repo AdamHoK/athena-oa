53769c7e45a6ba40f4e7316df13638c6e21cb402
Adam Hochschild <ahochs@mimi.cs.mcgill.ca>
2023-02-24 21:00:26 -0500

    Working backend

---
+++ athena-oa/server/server.js
@@ -0,0 +1,20 @@
+var athenaData = require("./athena-info.json");
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+app.get("/api/products", function (req, res) {
+    res.status(200).json(athenaData.products.at(0));
+});
